da86ed6301d35eaa42af3bd6946f204f5d3e1d4f
Alokpandey786 <pandayalok786@gmail.com>
2019-02-28 12:41:10 +0530

    Initial commit

---
+++ app/src/main/java/pandey/alok/twoaactivities/MainActivity.java
@@ -0,0 +1,74 @@
+package pandey.alok.twoaactivities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final EditText name1,message;
+        name1=findViewById(R.id.editText5);
+        message=findViewById(R.id.editText6);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String s1=name1.getText().toString().trim();
+                String s2=message.getText().toString().trim();
+                if(s1.equals("")||s2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this,"enter values first!!",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Intent sendmessage=new Intent(MainActivity.this,MainActivity.class);
+                    sendmessage.putExtra("name",s1);
+                    sendmessage.putExtra("message",s2);
+                    startActivity(sendmessage);
+
+                }
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
